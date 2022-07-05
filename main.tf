9d422956826bbfffbff027583a431ea564412b06
Bernardo <bernardo.vieira@dp6.com.br>
2022-07-05 13:19:15 -0300

    Adds main.tf with SNS, SQS and S3 services

---
+++ main.tf
@@ -0,0 +1,30 @@
+# SNS
+resource "aws_sns_topic" "tickets"{
+    name = "zendesk-tickets-topic"   
+}
+
+# SQS
+resource "aws_sqs_queue" "tickets_queue" {
+    name = "zendesk-tickets-queue"
+}
+
+
+# SNS Subscription
+resource "aws_sns_topic_subscription" "tickets_queue_to_topic" {
+    topic_arn = aws_sns_topic.tickets.arn
+    protocol = "sqs"
+    endponit = aws_sqs_queue.tickets_queue.arn
+}
+
+# S3
+resource "aws_s3_bucket" "s3_bucket_raw"{
+    name = "atendimento-tickets-raw"
+}
+
+resource "aws_s3_bucket" "s3_bucket_processed"{
+    name = "atendimento-tickets-processed"
+}
+
+resource "aws_s3_bucket" "s3_bucket_curated"{
+    name = "atendimento-tickets-curated"
+}
